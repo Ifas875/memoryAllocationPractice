306226ab6b29cfbf9aded27bad1c10e2cc9e04d4
Safi <safi.sharieff01@gmail.com>
2026-03-20 15:45:33 +0400

    Added setup

--- main.cpp
+++ main.cpp
@@ -9,9 +9,13 @@ u_int8_t heap[100];
 struct blockHeader{
   blockHeader *next;
   int size;
+  bool empty = false;
 };
 
 
 int main(){
+  blockHeader *start = new blockHeader();
+  start->size = 100-sizeof(blockHeader);
+  heap[0] = reinterpret_cast<u_int8_t>(start);
   return 0;
 }
