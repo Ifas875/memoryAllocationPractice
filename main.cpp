599a4b3d984df062b2b46720278209832aec5a39
Safi <safi.sharieff01@gmail.com>
2026-03-21 19:36:50 +0400

    Added maloc partially

--- main.cpp
+++ main.cpp
@@ -4,17 +4,46 @@
 
 
 struct blockHeader{
-  blockHeader *next;
+  blockHeader *next = nullptr;
   size_t size;
   bool empty = false;
 };
 
 alignas(blockHeader)  u_int8_t heap[100];
+blockHeader *start = nullptr;
+
+
+blockHeader * myMalloc(size_t required){
+  bool status = false;
+  // Searching for available block of memory
+  blockHeader *subject = start;
+  while(subject != nullptr){
+    if(subject->size >= required){
+      return subject;
+    }
+    else{
+      subject = subject->next;
+    }
+  }
+
+  // // Scenario if none of the available blocks matched the requirements
+  // // Condensing free blocks
+  // blockHeader *subject = start;
+  // while(subject != nullptr){
+    
+  // }
+  return nullptr;
+}
+
+
+void myFree(blockHeader *target){
+  return;
+}
 
 
 int main(){
   int offset = 0;
-  blockHeader *start = reinterpret_cast<blockHeader *>(&heap[offset]);
+  start = reinterpret_cast<blockHeader *>(&heap[offset]);
   start->empty = true;
   start->size = sizeof(heap) - sizeof(*start);
   offset += start->size;
