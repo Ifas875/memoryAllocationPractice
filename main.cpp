9982eb6852c96d035938aca9e301c5d8a8b1e6a5
Safi <safi.sharieff01@gmail.com>
2026-03-26 15:42:09 +0400

    Minor changes

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ blockHeader * myMalloc(size_t required){
     }
   }
 
-  // // Scenario if none of the available blocks matched the requirements
+  // // Scenario if none of the available block s matched the requirements
   // Need to redo entire malloc
   // // Condensing free blocks
   // blockHeader *subject = start;
